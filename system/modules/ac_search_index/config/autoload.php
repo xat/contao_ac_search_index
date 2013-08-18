53078d37bcc3849982db4de5406ebf69abe964d1
Simon Kusterer <simon@soped.com>
2013-08-18 17:04:52 +0200

    typo

--- system/modules/ac_search_index/config/autoload.php
+++ system/modules/ac_search_index/config/autoload.php
@@ -9,7 +9,7 @@ ClassLoader::addClasses(array
 	'AcSearchIndexCron'              => 'system/modules/ac_search_index/AcSearchIndexCron.php',
 	'AcSearchIndexHelper'            => 'system/modules/ac_search_index/AcSearchIndexHelper.php',
 	'ModuleMootoolsAcSearchIndex'    => 'system/modules/ac_search_index/ModuleMootoolsAcSearchIndex.php',
-	'ModuleJqueryAcSearchIndex'      => 'system/modules/ac_search_index/ModuleJQueryAcSearchIndex.php',
+	'ModuleJqueryAcSearchIndex'      => 'system/modules/ac_search_index/ModuleJqueryAcSearchIndex.php',
 ));
 
 TemplateLoader::addFiles(array
